d5552e51580da25e27cb86a943ce1971de88da67
thinkphat <nguyenthinhphat3009@gmail.com>
2026-04-12 14:24:59 +0700

    Refactor description

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -14,30 +14,30 @@ pcall(function()
         vim.keymap.set(m,l,r,{buffer=bufnr,desc=d})
       end
 
-      map('n',']c',gs.next_hunk,'next change')
-      map('n','[c',gs.prev_hunk,'previous change')
+      map('n',']c',gs.next_hunk,'Next Change')
+      map('n','[c',gs.prev_hunk,'Previous Change')
 
-      map({'n','v'},'<leader>gsh',gs.stage_hunk,'git stage hunk')
-      map('n','<leader>gsb',gs.stage_buffer,'git stage buffer')
+      map({'n','v'},'<leader>gsh',gs.stage_hunk,'Git Stage Hunk')
+      map('n','<leader>gsb',gs.stage_buffer,'Git Stage Buffer')
 
-      map({'n','v'},'<leader>grh',gs.reset_hunk,'git reset hunk')
-      map('n','<leader>grb',gs.reset_buffer,'git reset buffer')
+      map({'n','v'},'<leader>grh',gs.reset_hunk,'Git Reset Hunk')
+      map('n','<leader>grb',gs.reset_buffer,'Git Reset Buffer')
 
-      map('n','<leader>gph',gs.preview_hunk,'git preview hunk')
+      map('n','<leader>gph',gs.preview_hunk,'Git Preview Hunk')
 
-      map('n','<leader>gbl',function()
+      map('n','<leader>gb',function()
         gs.blame_line({full=true})
-      end,'git blame line')
+      end,'Git Blame Line')
 
-      map('n','<leader>gdf',gs.diffthis,'git diff file')
+      map('n','<leader>gdf',gs.diffthis,'Git Diff File')
       map('n','<leader>gdh',function()
         gs.diffthis('~')
-      end,'git diff head')
+      end,'Git Diff Head')
 
-      map('n','<leader>gtb',gs.toggle_current_line_blame,'toggle blame')
-      map('n','<leader>gtd',gs.toggle_deleted,'toggle deleted')
+      map('n','<leader>gtb',gs.toggle_current_line_blame,'Toggle Blame')
+      map('n','<leader>gtd',gs.toggle_deleted,'Toggle Deleted')
 
-      map('n','<leader>guh',gs.undo_stage_hunk,'undo stage')
+      map('n','<leader>guh',gs.undo_stage_hunk,'Undo Stage')
     end
   })
 end)
